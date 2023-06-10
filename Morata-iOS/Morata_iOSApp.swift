22f70c2334df6e99ccae0b2c4b0e45baecfe3009
Onnwen Cassitto <onnwen.cassitto@icloud.com>
2023-06-10 23:29:32 +0200

    Initial Commit

---
+++ Morata-iOS/Morata_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Morata_iOSApp.swift
+//  Morata-iOS
+//
+//  Created by Onnwen Cassitto on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Morata_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
